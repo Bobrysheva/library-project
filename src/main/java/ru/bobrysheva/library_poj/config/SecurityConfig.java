d82fae462fcd1a7b4b54f5fd13d77feff9d0d9ff
bobrisheva <bobrisheva@gmail.com>
2025-02-20 15:47:00 +0300

    filling SecurityConfig.java

--- src/main/java/ru/bobrysheva/library_poj/config/SecurityConfig.java
+++ src/main/java/ru/bobrysheva/library_poj/config/SecurityConfig.java
@@ -1,2 +1,45 @@
-package ru.bobrysheva.library_poj.config;public class SecurityConfig {
+package ru.bobrysheva.library_poj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers("/books/v1").hasRole("USER")
+                                .requestMatchers("/books/v2").hasRole("ADMIN")
+                                .requestMatchers("/books").hasRole("USER")
+                                .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = users
+                .username("admin")
+                .password("password")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
